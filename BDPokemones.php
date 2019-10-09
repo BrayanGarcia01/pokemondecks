8fa73505a17ee2335b6e2bf28c01ae887e86c2f5
Brayan <bsagrcia@unicauca.edu.co>
2019-10-08 23:34:14 -0500

    Pokemones

---
+++ BDPokemones.php
@@ -0,0 +1,13 @@
+<?php
+include "pokemon.php";
+
+$pokemones = array();
+$pokemones[0] = new Pokemon(1, "Charmander","charmander.png", 10,"fuego", 10, 1);
+$pokemones[1] = new Pokemon(2, "Charizard","charizard.png", 50,"fuego", 50, 3);
+$pokemones[2] = new Pokemon(3, "Clefairy","clefairy.png", 5,"normal", 5, 1)  ;
+$pokemones[3] = new Pokemon(4, "Psyduck","pokebola.png", 5,"agua", 5, 1)  ;
+$pokemones[4] = new Pokemon(5, "Abra","abra.png", 5,"psiquico", 5, 1)  ;
+$pokemones[5] = new Pokemon(6, "Caterpie","caterpie.png", 5,"bicho", 5, 1)  ;
+$pokemones[6] = new Pokemon(7, "Conkeldurr","conkeldurr.png", 5,"lucha", 5, 1);
+$pokemones[7] = new Pokemon(8, "Diglett","diglett.png", 5,"tierra", 5, 1)  ;
+    ?>
